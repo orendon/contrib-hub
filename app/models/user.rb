8a18121e039c395d1617f0b2c52684b2920e98f6
Oscar Rendon <oarendon@gmail.com>
2012-09-30 02:02:53 -0500

    Updated user resource.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ActiveRecord::Base
   attr_accessible :github_id, :name
 
+  has_many :repos
+
   def self.find_or_create_from(auth_data)
     login = auth_data["info"]["nickname"]
     token = auth_data["credentials"]["token"]
     name = auth_data["info"]["name"]
 
-    existing_user = find_by_github_id(login)
+    user = find_by_github_id(login)
 
-    if existing_user
-      existing_user.update(token: token)
+    if user
+      user.token = token
+      user.save!
     else
-      create! do |user|
-        user.github_id = login
-        user.name = name
-        user.token = token
-      end
+      user = User.create!(github_id: login, name: name, token: token)
     end
+    user
   end
 end

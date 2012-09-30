e1809083b9d7bc47eaf60690895f5aa45f3ba153
Oscar Rendon <oarendon@gmail.com>
2012-09-30 01:07:13 -0500

    added nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   attr_accessible :github_id, :name
 
   def self.find_or_create_from(auth_data)
-    login = auth_data["extra"]["login"]
+    login = auth_data["info"]["nickname"]
     token = auth_data["credentials"]["token"]
+    name = auth_data["info"]["name"]
 
     existing_user = find_by_github_id(login)
 
@@ -12,7 +13,7 @@ class User < ActiveRecord::Base
     else
       create! do |user|
         user.github_id = login
-        user.name = auth_data["info"]["name"]
+        user.name = name
         user.token = token
       end
     end

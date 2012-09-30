134beeb254785b54b9a7c124bd682ec61c20ac4e
Jean Pierre Guarin <escribimepues@gmail.com>
2012-09-29 22:29:51 -0500

    Added user page with info

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,6 +9,6 @@ class ProfilesController < ApplicationController
   private
     def get_user_info(user_token)
       github = Github.new basic_auth: 'gomayonqui:desarrollo1' #oauth_token: user_token
-      github.users.get
+      @user = github.users.get
     end
 end

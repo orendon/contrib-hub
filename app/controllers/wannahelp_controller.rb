ae73195a11d544c2924f5e6dd3030c7a54984121
Oscar Rendon <oarendon@gmail.com>
2012-09-30 04:16:47 -0500

    Wanna help feature basics.

--- app/controllers/wannahelp_controller.rb
+++ app/controllers/wannahelp_controller.rb
@@ -3,4 +3,16 @@ class WannahelpController < ApplicationController
   def index
     @repos = Repo.limit(5)
   end
+
+  def update
+    repo = Repo.find(params[:id])
+    star(repo.user, repo.name)
+    redirect_to user_wannahelp_index_path(current_user)
+  end
+
+  private
+    def star(user, repo)
+      github = Github.new basic_auth: 'gomayonqui:desarrollo1'
+      github.repos.starring.starred(owner: 'orendon', repo: 'ruby-shop')
+    end
 end

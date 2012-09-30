ad432c3fb404b626b1a628167af9111b7c59291f
Oscar Rendon <oarendon@gmail.com>
2012-09-30 07:25:20 -0500

    Added helper for repo status.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def get_css_status(user, repo)
+    return "i-help-button" if repo.is_being_helped_by(user)
+    repo.need_help ? "help-button" : "no-help-button"
+  end
 end

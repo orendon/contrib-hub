b29e00e3b0cb73e0058368908f05652c0b63b98b
Oscar Rendon <oarendon@gmail.com>
2012-09-29 19:46:04 -0500

    base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContribHub::Application.config.secret_token = '50574b3232c54a67a90c672733b0dadea06163a9aefe46cf0a35113b5e63f268cf6ca8b452d5bb5d6bb2cf823a3f902e5585040e44ed393de5a3c7dacbdcef46'

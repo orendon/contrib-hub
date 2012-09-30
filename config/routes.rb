bb7b81d79e07d0d47b38edcbae200e34e7cab770
Oscar Rendon <oarendon@gmail.com>
2012-09-29 21:35:21 -0500

    added controller for home page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ ContribHub::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  root :to => "home#index"
+  match "/auth/:provider/callback" => "sessions#create"
+
+  resources :profiles
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action

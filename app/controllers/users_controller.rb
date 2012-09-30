2cad1d291a23080b5fd5d62de9b60e1df604e66e
Jean Pierre Guarin <escribimepues@gmail.com>
2012-09-30 02:09:54 -0500

    Adding need help update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,11 @@ class UsersController < ApplicationController
 
   def show
     @user = get_info_for(current_user)
+    @user_repos = get_repos_list(current_user)
   end
 
   def needhelp
-
+    
   end
 
   def wannahelp
@@ -18,5 +19,10 @@ class UsersController < ApplicationController
     def get_info_for(user)
       github = Github.new oauth_token: user.token
       @user = github.users
+    end
+
+    def get_repos_list(user)
+      repos = Github::Repos.new
+      repos.all user: user.github_id
     end
 end

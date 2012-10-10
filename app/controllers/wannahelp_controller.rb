7a81030a8fd38adc2d44261a64b4f4c41f76d6bc
Oscar Rendon <oarendon@gmail.com>
2012-10-10 17:00:03 -0500

    Added filters for repos.

--- app/controllers/wannahelp_controller.rb
+++ app/controllers/wannahelp_controller.rb
@@ -1,6 +1,7 @@
 class WannahelpController < ApplicationController
   def index
-    @repos = Repo.get_repos_form_others(current_user)
+    @repos_search = Repo.search(build_filters(params))
+    @languages = Repo.get_languages
   end
 
   def update
@@ -12,6 +13,12 @@ class WannahelpController < ApplicationController
   end
 
   private
+    def build_filters(params)
+      filters = { "need_help_is_true" => true, "user_id_ne" => current_user.id }
+      filters.merge!(params[:search]) if params && params.has_key?(:search)
+      filters
+    end
+
     def star(user, repo)
       github = Github.new oauth_token: user.token
       github.repos.starring.starred(owner: user.github_id, repo: repo)

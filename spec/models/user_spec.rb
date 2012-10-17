5e97dbf1448497cd9272290b02ee40d393b8aa9a
Jean Pierre Guarin <escribimepues@gmail.com>
2012-10-17 14:13:03 -0500

    Added Basic spec user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,38 @@
 require 'spec_helper'
 
 describe User do
-  it "should validate required fields" do
-    user = FactoryGirl.build(:user)
-    user.should be_valid
+
+  describe "base" do
+    it "has a valid factory" do
+      FactoryGirl.build(:user).should be_valid
+    end
   end
+
+  describe "validations" do
+    before(:each) do
+      @user = FactoryGirl.build(:user)
+    end
+
+    it "is invalid without a github_id" do
+      @user.github_id = nil
+      @user.should_not be_valid
+    end
+
+    it "is invalid without a name" do
+      @user.name = nil
+      @user.should_not be_valid
+    end
+
+    it "is invalid without a token" do
+      @user.token = nil
+      @user.should_not be_valid
+    end
+
+    it "is invalid with a duplicate github_id" do
+      FactoryGirl.create(:user, github_id: 1)
+      FactoryGirl.build(:user, github_id: 1).should_not be_valid
+    end
+  end
+
+
 end

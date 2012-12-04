ff34a5d281190077a89adb483b7df53320c6d0a5
Oscar Rendon <oarendon@gmail.com>
2012-12-04 16:59:22 -0500

    Some refactoring, github_id is more accurate for tagging.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,24 @@
 class User < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :name, use: :slugged
+
   attr_accessible :github_id, :name, :token, :location, :latitude, :longitude
 
-  # relations
+  ## friendly url
+  friendly_id :github_id, use: :slugged
+
+  ## relations
   has_many :repos
   has_many :helped_repos
 
-  # geolocation
+  ## geolocation
   geocoded_by :location
   after_validation :geocode, :if => :location_changed?
 
-  # validations
+  ## validations
   validates :github_id, :name, :token, :presence => true
   validates :github_id, :uniqueness => true
+
+  ## instance methods
 
   def update_repo_status(repo)
     if repo.is_being_helped_by?(self)
@@ -23,21 +28,27 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.find_or_create_from(auth_data)
-    login = auth_data["info"]["nickname"]
-    token = auth_data["credentials"]["token"]
-    name = auth_data["info"]["name"]
-    location = auth_data["extra"]["raw_info"]["location"]
+  ## class methods
 
-    user = find_by_github_id(login)
+  class << self
 
-    if user
-      user.token = token
-      user.location = location
-      user.save!
-    else
-      user = User.create!(github_id: login, name: name, token: token, location: location)
+    def self.find_or_create_from(auth_data)
+      login = auth_data["info"]["nickname"]
+      token = auth_data["credentials"]["token"]
+      name = auth_data["info"]["name"]
+      location = auth_data["extra"]["raw_info"]["location"]
+
+      user = find_by_github_id(login)
+
+      if user
+        user.token = token
+        user.location = location
+        user.save!
+      else
+        user = User.create!(github_id: login, name: name, token: token, location: location)
+      end
+      user
     end
-    user
+
   end
 end

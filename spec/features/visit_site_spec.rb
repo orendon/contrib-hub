a9a0bdcc051931e90ea8387c4ec55e77313ae480
jlstr <jlecampana@gmail.com>
2012-12-05 16:47:31 -0500

    Correcting landing spec

--- spec/features/visit_site_spec.rb
+++ spec/features/visit_site_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 feature "user visits site" do
   it "displays landing page" do
     visit root_url
-    page.should have_content("We Love Open Source")  
+    page.should have_content("Open source lovers")
+    page.should have_content("making easier to find cool repos to hack on!")  
   end
 end

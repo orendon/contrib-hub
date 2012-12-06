974a2eb1e4e137a9fac78742db32c6731283ee62
Raison Dsouza <raisondsouza@gmail.com>
2012-12-06 23:04:58 +0530

    avoid repeating FactoryGirl; updated specs to latest expectation syntax

--- spec/models/helped_repos.rb
+++ spec/models/helped_repos.rb
@@ -4,28 +4,26 @@ describe HelpedRepo do
 
   describe "base" do
     it "has a valid factory" do
-      FactoryGirl.build(:helped_repo).should be_valid
+      expect(build(:helped_repo)).to be_valid
     end
   end
 
   describe "validations" do
-    before(:each) do
-      @helped_repo = FactoryGirl.build(:helped_repo)
-    end
+      let(:helped_repo) { build(:helped_repo) }
 
     it "is invalid without a really_helping" do
-      @helped_repo.really_helping = nil
-      @helped_repo.should_not be_valid
+      helped_repo.really_helping = nil
+      expect(helped_repo).to_not be_valid
     end
 
     it "is invalid without a repo" do
-      @helped_repo.repo = nil
-      @helped_repo.should_not be_valid
+      helped_repo.repo = nil
+      expect(helped_repo).to_not be_valid
     end
 
     it "is invalid without a user" do
-      @helped_repo.user = nil
-      @helped_repo.should_not be_valid
+      helped_repo.user = nil
+      expect(helped_repo).to_not be_valid
     end
 
   end

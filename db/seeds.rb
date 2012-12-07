6e9e7e997be9c8130958a39da971a4c645bf3824
Jean Pierre Guarin <escribimepues@gmail.com>
2012-12-07 10:27:29 -0500

    Add development seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env.development? && User.count == 0
+	require 'faker'
+	require 'factory_girl_rails'
+	include FactoryGirl::Syntax::Methods
+
+	puts 'Inserting sample users'
+
+	20.times do
+		user = create :user
+		user.save!
+		10.times do
+			repo = create :repo, user: user, need_help: [true, false].sample
+		end
+	end
+
+end

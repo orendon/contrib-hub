542fd3c8ca1b59346dd994cfd586dab2e9ecd733
Oscar Rendon <oarendon@gmail.com>
2012-12-11 09:18:32 -0500

    helper to build map url using google maps.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,22 @@ module UsersHelper
   def status(repo)
     'hidden' unless repo[:need_help]
   end
+
+  def build_static_map(user)
+    user_marker = "&markers=color:blue%7Clabel:Y%7C#{user.latitude},#{user.longitude}" unless user.location.nil?
+    helping_markers = get_repos_markers(user.get_helped_repos_coords)
+    size = "250x250"
+    map_url = "http://maps.google.com/maps/api/staticmap?&size=#{size}&maptype=roadmap#{user_marker}#{helping_markers}&sensor=false"
+    image_tag map_url
+  end
+
+  private
+
+  def get_repos_markers(coords)
+    markers = ""
+    coords.each do |coord|
+      markers << "&markers=color:Red%7Clabel:R%7C#{coord[:latitude]},#{coord[:longitude]}"
+    end
+    markers
+  end
 end

e6a9aee99a937075d69df5fc619ec10f5cb80bbc
Oscar Rendon <oarendon@gmail.com>
2012-12-24 15:41:15 -0500

    Adds one marker on map foreach user needing help.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,5 @@
-var stockholm = new google.maps.LatLng(59.32522, 18.07002);
-var parliament = new google.maps.LatLng(59.327383, 18.06747);
+var map_center = new google.maps.LatLng(19.311143,-39.199219);
+//var medellin = new google.maps.LatLng(6.29, -75.54);
 var marker;
 var map;
 
@@ -9,18 +9,39 @@ function initialize_map() {
   var mapOptions = {
     zoom: 2,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: stockholm
+    center: map_center
   };
 
   map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+  loadUsersData();
+}
 
+function loadUsersData() {
+  $.getJSON('/users_map.json', function(data){
+    $.each(data, function(key, value){
+      user_location = new google.maps.LatLng(value.latitude, value.longitude);
+      addMarker(user_location);
+    });
+  });
+}
+
+function addMarker(place) {
   marker = new google.maps.Marker({
     map:map,
     draggable:true,
     animation: google.maps.Animation.DROP,
-    position: parliament
+    position: place
   });
-  google.maps.event.addListener(marker, 'click', toggleBounce);
+  google.maps.event.addListener(marker, 'click', bounceOnce);
+}
+
+function bounceOnce() {
+  marker.setAnimation(google.maps.Animation.BOUNCE);
+  setInterval(stopBounce, 1000);
+}
+
+function stopBounce() {
+  marker.setAnimation(null)
 }
 
 function toggleBounce() {

ef5926c136129b713b4dde28c672d51ccee339a4
Oscar Rendon <oarendon@gmail.com>
2012-12-24 12:44:40 -0500

    Fix failing tests.

--- spec/features/visit_site_spec.rb
+++ spec/features/visit_site_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature "user visits site" do
   it "displays landing page" do
     visit root_url
-    page.should have_content("Open source lovers")
-    page.should have_content("making easier to find cool repos to hack on!")  
+    page.should have_content("Open Source Lovers")
+    page.should have_content("Making easier to find cool repos to hack on!")
   end
 end

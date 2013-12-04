e1fd693d4a8525123a23ae9bb09ed2376a95fa57
Oscar Rendon <oarendon@gmail.com>
2013-12-04 14:40:13 -0500

    Avoid bootstraping Rails application during compilation, avoid heroku precompiling assets issue.

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -65,8 +59,6 @@ module ContribHub
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    # autoload /lib folder
-    # config.autoload_paths += %W(#{config.root}/lib)
-    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.assets.initialize_on_precompile = false
   end
 end

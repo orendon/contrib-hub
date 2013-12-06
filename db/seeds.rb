d2ac276641223d1e487138185b00451c79c6cd87
Oscar Rendon <oarendon@gmail.com>
2013-12-06 02:10:26 -0500

    Refactor, extract random languajes and help status to factory.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 if Rails.env.development? && User.count == 0
 	require 'faker'
@@ -17,7 +13,7 @@ if Rails.env.development? && User.count == 0
 		user = create :user
 		user.save!
 		10.times do
-			repo = create :repo, user: user, need_help: [true, false].sample, language: ['Ruby', 'Perl', 'C', 'PHP', 'Java', 'Ruby', 'JavaScript'].sample
+			repo = create :repo, user: user
 		end
 	end
 
--- spec/factories/repos.rb
+++ spec/factories/repos.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :repo do
     association :user
-    github_url "https://github.com/orendon/contrib-hub"
+    github_url { Faker::Internet.url }
     sequence(:github_id) {|n| n }
     name { Faker::Lorem.word }
-    need_help true
+    need_help { [true, false].sample }
     description { Faker::Lorem.paragraph }
     user_description { Faker::Lorem.paragraph }
-    language "Ruby"
+    language { ['Ruby', 'Perl', 'C', 'PHP', 'Java', 'Ruby', 'JavaScript'].sample }
   end
 end

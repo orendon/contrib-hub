25f8911a7276c694bc4f26579b40862330b48325
Oscar Rendon <oarendon@gmail.com>
2013-12-15 10:19:42 -0500

    Conditional loading for test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
-require 'simplecov'
-SimpleCov.start
+if ENV['SIMPLECOV']
+  require 'simplecov'
+  SimpleCov.start
+end
+
+if ENV['COVERALLS']
+  require 'coveralls'
+  Coveralls.wear!
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'

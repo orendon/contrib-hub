9574d325518b341040112644670582ee2791c097
Adrian Rangel <adrian.rangel@gmail.com>
2014-01-02 20:37:09 -0600

    use github secret instead of hardcoded

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :github, '6326b3fb4d56e37d85d0', '8c203446caedec8fd08d9a1c60067e5da08257ae'
+    provider :github, '6326b3fb4d56e37d85d0', ENV['GITHUB_SECRET']
   else
     provider :github, '0b352782a5c08bb12c7e', 'dfb074b732d1694c19f638cfc2236aba7ba567cc'
   end

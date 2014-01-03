2137509ce4ee1340d2ddaeb337435cf32979617b
Adrian Rangel <adrian.rangel@gmail.com>
2014-01-02 19:06:01 -0600

    switch unless for if
    unless confuses me when there is an else :p, also is it 'safe' to have the production Client Secret there?

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  unless Rails.env.production?
-    provider :github, '0b352782a5c08bb12c7e', 'dfb074b732d1694c19f638cfc2236aba7ba567cc'
-  else
+  if Rails.env.production?
     provider :github, '6326b3fb4d56e37d85d0', '8c203446caedec8fd08d9a1c60067e5da08257ae'
+  else
+    provider :github, '0b352782a5c08bb12c7e', 'dfb074b732d1694c19f638cfc2236aba7ba567cc'
   end
 end
 

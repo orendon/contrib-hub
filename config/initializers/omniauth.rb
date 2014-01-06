ffd27321d4058cbff4f8f08aa2b8d91ed1a7e6fe
Adrian Rangel <adrian.rangel@gmail.com>
2014-01-06 15:43:49 -0600

    use github['key']

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :github, '6326b3fb4d56e37d85d0', ENV['GITHUB_SECRET']
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   else
     provider :github, '0b352782a5c08bb12c7e', 'dfb074b732d1694c19f638cfc2236aba7ba567cc'
   end

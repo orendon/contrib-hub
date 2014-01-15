fe154731bd437970126cfd8355b8f5af68e06cdc
Oscar Rendon <oarendon@gmail.com>
2014-01-14 19:55:35 -0500

    Add specs for MapUrlBuilder, refactor.

--- app/services/map_url_builder.rb
+++ app/services/map_url_builder.rb
@@ -1,42 +1,47 @@
+# Build a URL using Google Static Maps API
 class MapUrlBuilder
+
+  USER_PROFILE_DIMENSIONS   = '350x350'
+  SEARCH_RESULTS_DIMENSIONS = '250x250'
 
   def initialize(size)
     @size, @markers = size, []
   end
 
-  def self.user_map(user)
-    map = MapUrlBuilder.new('350x350')
+  def self.user_profile_map(user)
+    map = MapUrlBuilder.new(USER_PROFILE_DIMENSIONS)
     map.add_marker('blue', user.coords_and_github_id)
     map.add_markers('red', user.helped_repos_coords)
     map.url
   end
 
-  def self.search_map(repos)
-    MapUrlBuilder.new('250x250').add_markers('red', repos.map(&:coords_and_github_id)).url
+  def self.search_results_map(repos)
+    repos_map_data = repos.map { |repo| repo.user.coords_and_github_id }
+
+    MapUrlBuilder.new(SEARCH_RESULTS_DIMENSIONS).add_markers('red', repos_map_data).url
   end
 
-  def add_marker(colour, coords_and_github_id)
-    @markers << build_string(colour, coords_and_github_id[:latitude], coords_and_github_id[:longitude])
+  def add_marker(colour, geolocation_data)
+    @markers << build_marker_params(colour, geolocation_data[:latitude], geolocation_data[:longitude])
     self
   end
 
-  def add_markers(colour, items)
-    items.each{ |i| @markers << build_string( colour, i[:latitude],
-      i[:longitude] ) }
+  def add_markers(colour, geolocation_data)
+    geolocation_data.each { |data| add_marker(colour, data) }
     self
   end
 
   def url
-    map_url = "http://maps.google.com/maps/api/staticmap?&size=#{@size}&" +
-      "maptype=roadmap#{@markers.join}&sensor=false"
-    map_url << "&zoom=13" if map_url.scan(/markers/).count == 1
-    map_url
+    google_map_url = "http://maps.google.com/maps/api/staticmap?&size=%{size}&maptype=roadmap%{markers}&sensor=false"
+
+    repos_map_url = google_map_url % { size: @size, markers: @markers.join }
+    repos_map_url << "&zoom=13" if @markers.size == 1
+    repos_map_url
   end
 
   private
 
-  def build_string(colour, latitude, longitude)
-    return '' if [latitude, longitude].any?{|l| l.nil? }
+  def build_marker_params(colour, latitude, longitude)
     "&markers=color:#{colour}%7Clabel:O%7C#{latitude},#{longitude}"
   end
 
---
+++ spec/services/map_url_builder_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe MapUrlBuilder do
+  let(:data) { [ {latitude: 67, longitude: 67}, {latitude: 40, longitude: 40} ] }
+  let(:subject) { MapUrlBuilder.new('100x100') }
+  let(:user) { build(:user) }
+  let(:repos) { [ build(:repo), build(:repo) ] }
+
+  describe "#add_marker" do
+    it "builds one single marker correctly" do
+      map_url = "http://maps.google.com/maps/api/staticmap?&size=100x100&maptype=roadmap&markers=color:red%7Clabel:O%7C67,67&sensor=false&zoom=13"
+      expect( subject.add_marker('red', data.first).url ).to eq(map_url)
+    end
+  end
+
+  describe "#add_markers" do
+    it "builds one single marker correctly" do
+      map_url = "http://maps.google.com/maps/api/staticmap?&size=100x100&maptype=roadmap&markers=color:blue%7Clabel:O%7C67,67&markers=color:blue%7Clabel:O%7C40,40&sensor=false"
+      expect( subject.add_markers('blue', data).url ).to eq(map_url)
+    end
+  end
+
+  describe ".user_profile_map" do
+    it "renders user profile map specific dimensions" do
+      expect( MapUrlBuilder.user_profile_map(user) ).to match /size=350x350/
+    end
+  end
+
+  describe ".search_results_map" do
+    it "renders search results map specific dimensions" do
+      expect( MapUrlBuilder.search_results_map(repos) ).to match /size=250x250/
+    end
+  end
+end

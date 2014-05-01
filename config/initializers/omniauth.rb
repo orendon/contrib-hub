8fe35b7b8a4e73cc1c243347b87fa2ce2c13e46c
Oscar Rendon <orendon@gmail.com>
2014-05-01 18:09:51 -0500

    Move github key/secret to ENV vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.production?
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-  else
-    provider :github, '0b352782a5c08bb12c7e', 'dfb074b732d1694c19f638cfc2236aba7ba567cc'
-  end
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
 
 OmniAuth.config.on_failure = HomeController.action(:index)

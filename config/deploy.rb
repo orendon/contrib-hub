7be38001d12bf73c86a401abb9f51ab794db190a
Oscar Rendon <orendon@gmail.com>
2014-05-01 15:44:30 -0500

    Link secrets.yml during cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :format, :pretty
 # Default value for :pty is false
 # set :pty, true
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}

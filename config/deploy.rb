b3b251a21ed0a21a5157f6183712bb709f2932fd
Oscar Rendon <orendon@gmail.com>
2014-05-01 14:32:32 -0500

    Update plugins, username and host

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,17 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'contribhub'
+set :repo_url, 'git@github.com:orendon/contrib-hub.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/orendon/apps/contribhub'
 
-# Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+set :format, :pretty
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
 # set :log_level, :debug
 
 # Default value for :pty is false
@@ -31,8 +26,8 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 10
+set :rbenv_ruby, '2.1.1'
 
 namespace :deploy do
 

b08203ea5aa2aed5083a7e74272d46d8329fad21
Jean Pierre Guarin <escribimepues@gmail.com>
2014-05-11 18:53:37 -0500

    Create rake task at sunday midnight

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,5 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+set :output, 'log/whenever.log'
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
+every :monday, at: '12am' do
+  rake 'sync:all'
+end

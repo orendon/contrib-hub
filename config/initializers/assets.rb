fa1ca34a71cf7779edd2213a43bfa1d5997473f2
Al Snow <jasnow@hotmail.com>
2015-12-18 12:25:56 -0500

    Upgraded sprockets-rails gem - included new assets.rb initializer

---
+++ config/initializers/assets.rb
@@ -0,0 +1,9 @@
+Rails.application.config.assets.precompile += %w(
+                                                  bootstrap.min.css
+                                                  bootstrap-responsive.min.css
+                                                  contribhub.js
+                                                  map.js
+                                                  modernizr-2.7.1.js
+                                                  profile.css
+                                                  style.css
+                                              )

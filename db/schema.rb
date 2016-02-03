a8b2c5b7e40cfaac86057b8d30afb80b7dfc47f4
Al Snow <jasnow@hotmail.com>
2016-02-03 10:42:33 -0500

    New Gemfile.lock and db/schedma.rb files

--- db/schema.rb
+++ db/schema.rb
@@ -13,30 +13,30 @@
 
 ActiveRecord::Schema.define(version: 20150517195639) do
 
-  create_table "error_logs", force: true do |t|
+  create_table "error_logs", force: :cascade do |t|
     t.string   "message"
     t.string   "action"
     t.text     "backtrace"
     t.string   "extras"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "helped_repos", force: true do |t|
+  create_table "helped_repos", force: :cascade do |t|
     t.integer  "repo_id"
     t.integer  "user_id"
     t.boolean  "really_helping"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
-  create_table "repos", force: true do |t|
+  create_table "repos", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "name"
     t.string   "github_url"
     t.boolean  "need_help"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "github_id"
     t.string   "full_name"
     t.text     "description"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20150517195639) do
     t.boolean  "fork"
   end
 
-  create_table "taggings", force: true do |t|
+  create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
     t.string   "taggable_type"
@@ -63,19 +63,19 @@ ActiveRecord::Schema.define(version: 20150517195639) do
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
   add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
 
-  create_table "tags", force: true do |t|
+  create_table "tags", force: :cascade do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "github_id"
     t.string   "name"
     t.string   "token"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "location"
     t.float    "latitude"
     t.float    "longitude"
